0ac370056e15484f6d67cff1504cabb58d94166d
Alumno <claudia.cordova@tecsup.edu.pe>
2017-08-22 11:33:30 -0500

    Inicio de Proyecto 2

---
+++ lab2ExampleGitHub1/src/main/java/com/tecsup/lab2/git/controller/StudentController.java
@@ -0,0 +1,13 @@
+package com.tecsup.lab2.git.controller;
+
+/**
+ * Hello world!
+ *
+ */
+public class StudentController 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ lab2ExampleGitHub1/src/test/java/com/tecsup/lab2/git/controller/StudentControllerTest.java
@@ -0,0 +1,38 @@
+package com.tecsup.lab2.git.controller;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class StudentControllerTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public StudentControllerTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( StudentControllerTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
